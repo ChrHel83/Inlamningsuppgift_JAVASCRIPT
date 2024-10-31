f8eebff60962dc5e794fa95e21beb70ff39bda78
ChrHel83 <christian.hellberg83@gmail.com>
2024-10-31 10:51:02 +0100

    Subscribe-sektion nästan klar designmässigt

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,16 +1,23 @@
 import "./subscribe.css";
 
 const Subscribe = () => {
-    return ( 
-<section className="subscribe">
-    <div className="container">
-        <div className="content">
-            <img src="./public/bell.svg" alt="Bell" />
-            <span>Subscribe to our newsletter <p>
-            to stay informed about latest updates</p></span>
-        </div>
-    </div>
-</section>     );
+    return (
+        <section className="subscribe">
+            <div className="container">
+                <div className="content">
+                    <img src="./public/bell.svg" alt="Bell" />
+                    <span className="std-txt">Subscribe to our newsletter</span>
+                    <span className="long-txt">Subscribe to our newsletter to stay informed about latest updates</span>
+
+                </div>
+                <div className="input-subscribe">
+                <i className="fa-regular fa-envelope envelope"></i>
+                <input type="email" className="input-email" placeholder="Your email" />
+                <div className="btn-subscribe">Subscribe</div>
+                </div>
+            </div>
+        </section>
+    );
 }
- 
+
 export default Subscribe;
