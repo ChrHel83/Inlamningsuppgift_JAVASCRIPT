28d2abd9d82036122690efed653d66a3ab28c7d4
Christian Hellberg <christian.hellberg83@gmail.com>
2024-11-13 07:34:53 +0100

    Subscribe-formulär justerat med koppling till API och bekräftelseruta

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,13 +1,50 @@
+import React, { useState } from "react";
 import "./subscribe.css";
 
 const Subscribe = () => {
+    const [formInput, setFormInput] = useState({ email: "" });
+    const [submitted, setSubmitted] = useState(false);
+    const [errors, setErrors] = useState({});
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormInput({ ...formInput, [name]: value });
+    };
 
-const handleClick = () => {
-    console.log("Clicker")
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        // Validering av formInput innan nästa steg...
 
+        fetch("https://win24-assignment.azurewebsites.net/api/forms/subscribe", {
+            method: "post",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(formInput),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    setSubmitted(true);
+                    setFormInput({ email: "" });
+                }
+            })
+            .catch((err) => {
+                // console.log(err)
+                console.log("Något gick fel");
+            });
+    };
+
+    if (submitted) {
+        return (
+            <div className="container">
+                <div className="subscribe-msg">
+                    <h1>Thank you for subscribing</h1>
+                    {/* <button className="message-btn">OK</button> */}
+                </div>
+            </div>
+        );
+    }
 
     return (
         <section className="subscribe">
@@ -16,20 +53,18 @@ const handleClick = () => {
                     <img src="./public/bell.svg" alt="Bell" />
                     <span className="std-txt">Subscribe to our newsletter</span>
                     <span className="long-txt">Subscribe to our newsletter to stay informed about latest updates</span>
-
-                </div>
-             
-                <div className="input-subscribe">
-                <i className="fa-regular fa-envelope envelope"></i>
-                <input type="email" className="input-email" placeholder="Your email" />
-                <div className="btn-subscribe" onClick={handleClick}>Subscribe</div>
                 </div>
 
- 
-             
+                <form onSubmit={handleSubmit} className="form-subscribe" noValidate>
+                    <i className="fa-regular fa-envelope envelope"></i>
+                    <input type="email" name="email" className="input-email" onChange={handleChange} required value={formInput.email} placeholder="Your Email" />
+                    <button className="btn-subscribe" type="submit">
+                        Subscribe
+                    </button>
+                </form>
             </div>
         </section>
     );
-}
+};
 
 export default Subscribe;
